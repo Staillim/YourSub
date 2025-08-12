6a17b776dad8befcb229242e76edbbc2690eb9b9
Staillim <Staillimyts@gmail.com>
2025-08-11 19:09:52 -0500

    Update monetag-ads.tsx

--- src/components/monetag-ads.tsx
+++ src/components/monetag-ads.tsx
@@ -14,24 +14,16 @@ export function MonetagAds() {
     script2.src = 'https://vemtoutcheeg.com/400/9688580';
     document.body.appendChild(script2);
 
-    // Tercer script
-    const script3 = document.createElement('script');
-    script3.src = 'https://groleegni.net/401/9688582';
-    document.body.appendChild(script3);
-
-    // Cuarto script
-    const script4 = document.createElement('script');
-    script4.src = 'https://gizokraijaw.net/401/9688583';
-    document.body.appendChild(script4);
+    
 
     // Limpieza al desmontar
     return () => {
       document.body.removeChild(script1);
       document.body.removeChild(script2);
-      document.body.removeChild(script3);
-      document.body.removeChild(script4);
+      
     };
   }, []);
 
   return null;
 }
+
